e659a2c53c25fdfbc56e354d2ab03fb63e6b8daf
Magic Hua <hongery16888@qq.com>
2015-07-11 02:15:12 +0800

    更新框架

---
+++ app/src/main/java/iori/hdoctor/activity/pager/IndexPager.java
@@ -0,0 +1,44 @@
+package iori.hdoctor.activity.pager;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import iori.hdoctor.R;
+import iori.hdoctor.activity.base.BasePager;
+
+/**
+ * Created by Administrator on 2015/7/10.
+ */
+public class IndexPager extends BasePager{
+
+    private View view;
+    private Context context;
+
+    private boolean isInit = false;
+
+    public IndexPager(Context ct) {
+        super(ct);
+        context = ct;
+    }
+
+    @Override
+    public View initView(LayoutInflater inflater) {
+        view = inflater.inflate(R.layout.pager_index, null);
+        return view;
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void onResume() {
+        if (!isInit) {
+            initData();
+            isInit = !isInit;
+        }
+    }
+
+}
